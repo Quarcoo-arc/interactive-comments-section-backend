5c5ffddbfdd3b021df599437ed8098e352a2970c
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-11-01 04:36:24 +0000

    Add GraphQL type definition for User model

---
+++ src/models/User.ts
@@ -0,0 +1,10 @@
+import { builder } from "../builder";
+
+builder.prismaObject("User", {
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    name: t.exposeString("name"),
+    email: t.exposeString("email"),
+    comments: t.relation("comments"),
+  }),
+});
